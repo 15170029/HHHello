f99ab91935f6a66f4ad57c2d1d02c6be6629bc49
Aleksey Domrachev <aleksei.domrachev@mail.ru>
2019-04-01 14:15:51 +0500

    maven

---
+++ HHHello/src/main/java/HHHello/HHHello/App.java
@@ -0,0 +1,13 @@
+package HHHello.HHHello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
